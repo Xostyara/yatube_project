715092718adaa6530f934c5bb263ed186b35f1c3
Xostyara@gmail.com <Xostyara@gmail.com>
2022-07-29 16:45:20 +0300

    разбиты шаблоны по разным файлам

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,16 @@
+from re import template
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 # Главная страница
-def index(request):    
-    return HttpResponse('Всем привет! Это Главная страница')
+def index(request): 
+    template = 'posts/index.html'   
+    return render(request, template)
 
 
 # Страница с постами группы
 def group_posts(request, slug):
-    return HttpResponse(f'Это страница groups {slug}')
+    template = 'posts/group_list.html'
+    return render(request, template, slug)
 
 
